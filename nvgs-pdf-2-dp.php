8b63d4cd2c39bb4eea26f872222617aba809361a
Austin Reilly <areilly@alphaworks.tech>
2022-02-18 10:39:56 -0800

    git init

---
+++ nvgs-pdf-2-dp.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The plugin
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              SAE
+ * @since             1.0.0
+ * @package           pdf-2-db
+ *
+ * @wordpress-plugin
+ * Plugin Name:       pdf-2-db
+ * Plugin URI:        pdf-2-db
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            SAE
+ * Author URI:        SAE
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       pdf-2-db
+ * Domain Path:       /languages
+ */
